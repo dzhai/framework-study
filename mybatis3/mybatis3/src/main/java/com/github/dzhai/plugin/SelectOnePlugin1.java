02c4c3a12259089c4c8ffecda2e60a9739175916
dzhai <347112281@qq.com>
2016-06-28 19:10:03 +0800

    update

---
+++ mybatis3/mybatis3/src/main/java/com/github/dzhai/plugin/SelectOnePlugin1.java
@@ -0,0 +1,39 @@
+package com.github.dzhai.plugin;
+
+import java.util.Properties;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+@Intercepts({@Signature(
+		type= Executor.class,
+		method = "query",
+		args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
+public class SelectOnePlugin1 implements Interceptor {
+
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		System.out.println("1111111111111111111111111");
+		return invocation.proceed();
+	}
+
+	@Override
+	public Object plugin(Object target) {
+		// TODO Auto-generated method stub
+		return Plugin.wrap(target, this);
+	}
+
+	@Override
+	public void setProperties(Properties properties) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
